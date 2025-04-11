48faeee7bfb27d45adc887d1d924b5980235e4ec
VNZMichaelDev <maikermicha15@gmail.com>
2025-04-11 07:49:16 +0200

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,11 +1,9 @@
-// ADVERTENCIA: Incluir claves directamente en el código es un riesgo de seguridad
-// Cualquier persona que acceda a tu sitio web podrá ver estas claves
+// js/supabase-config.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm"
 
-// Reemplaza estos valores con tus claves reales de Supabase
-const supabaseUrl = "https://buuwyzypfkvkrbranisy.supabase.co"
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dXd5enlwZmt2a3JicmFuaXN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNDI4MDcsImV4cCI6MjA1OTkxODgwN30.AlsLQ12yHxrXH8zb1dmERBDiWh1WDW9K_ePE7ipz5Uc"
+// Usar variables de entorno que configuraremos en Vercel
+const supabaseUrl = window.SUPABASE_URL || "url_por_defecto"
+const supabaseAnonKey = window.SUPABASE_ANON_KEY || "clave_por_defecto"
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
